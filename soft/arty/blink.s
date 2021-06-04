99e32f78653ee42a5f8d77d171a24709914c5022
tux3 <barrdetwix@gmail.com>
2021-06-04 21:11:35 +0200

    Add simple blinky RV64IC program for the Arty board

---
+++ soft/arty/blink.s
@@ -0,0 +1,17 @@
+.equ    GPIO_BASE, 0x10000000000
+.globl _start
+
+_start:
+	li s1, GPIO_BASE
+	li s2, 1
+
+loop:
+	li s3, 10*1000*1000
+	sb s2, 0(s1)
+	xor s2, s2, 1
+
+count_loop:
+	add s3, s3, -1
+    bnez s3, count_loop
+	j loop
+
